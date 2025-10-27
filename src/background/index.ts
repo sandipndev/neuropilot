ba1c1e2683f7cc24fd013da3cd3bdef8f899fef2
Sandipan Dey <hey@sandipan.dev>
2025-10-28 04:47:36 +0530

    feat: scheduler updates

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,23 +1,70 @@
 import PQueue from "p-queue"
 
-import { backgroundInferenceTasks } from "./inference"
+import garbageCollectionTask from "~background/garbage-collection"
+import activitySummaryInferenceTask from "~background/inference/activity-summary"
+import doomscrollingDetectionTask from "~background/inference/doomscrolling"
+import focusInferenceTask from "~background/inference/focus"
+import pulseInferenceTask from "~background/inference/pulse"
+import quizQuestionsInferenceTask from "~background/inference/quiz-questions"
+import websiteSummarizerTask from "~background/inference/website-summarizer"
 
-const TASK_CONCURRENCY = 2
+const TASK_CONCURRENCY = 1
+const continuousTasksQueue = new PQueue({ concurrency: TASK_CONCURRENCY })
+const cronTasksQueue = new PQueue({ concurrency: TASK_CONCURRENCY })
 
-const queue = new PQueue({ concurrency: TASK_CONCURRENCY })
+// to run frequently
+const backgroundInferenceTasks = [
+  activitySummaryInferenceTask,
+  doomscrollingDetectionTask,
+  focusInferenceTask,
+  websiteSummarizerTask
+]
 
-const scheduleBackgroundInferenceTasks = async () => {
-  for (const task of backgroundInferenceTasks) {
-    queue.add(async () => task())
-  }
+const enqueueTask = (taskFn: () => Promise<any>, queue: PQueue) => {
+  console.log("Enqueuing task:", taskFn.name)
+  queue.add(async () => {
+    try {
+      await taskFn()
+    } catch (err) {
+      console.error("Task failed:", err)
+    }
+  })
 }
 
-queue.add(() => scheduleBackgroundInferenceTasks())
-queue.on("idle", async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
+// run inference loop continuously
+const scheduleBackgroundInferenceTasks = async () => {
+  for (const task of backgroundInferenceTasks)
+    enqueueTask(task, continuousTasksQueue)
+}
+
+continuousTasksQueue.add(() => scheduleBackgroundInferenceTasks())
+continuousTasksQueue.on("idle", async () => {
+  // wait a second to avoid issues and again continue
+  await new Promise((r) => setTimeout(r, 1000))
   scheduleBackgroundInferenceTasks()
 })
 
+// alarms for background tasks
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.create("pulse-task", { periodInMinutes: 5 })
+  chrome.alarms.create("quiz-task", { periodInMinutes: 2 })
+  chrome.alarms.create("garbage-collection-task", { periodInMinutes: 60 * 24 })
+})
+chrome.alarms.onAlarm.addListener((alarm) => {
+  switch (alarm.name) {
+    case "pulse-task":
+      enqueueTask(pulseInferenceTask, cronTasksQueue)
+      break
+    case "quiz-task":
+      enqueueTask(quizQuestionsInferenceTask, cronTasksQueue)
+      break
+    case "garbage-collection-task":
+      enqueueTask(garbageCollectionTask, cronTasksQueue)
+      break
+  }
+})
+
+// side panel open on icon click
 chrome.action.onClicked.addListener((tab) => {
   chrome.sidePanel.open({ windowId: tab.windowId })
 })

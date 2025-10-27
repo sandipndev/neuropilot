1dd80fb691fdb2c42889b9a1c759077243754894
Sandipan Dey <hey@sandipan.dev>
2025-10-27 23:16:25 +0530

    refactor: scheduler

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,4 +1,23 @@
-import "./inference"
+import PQueue from "p-queue"
+
+import { backgroundInferenceTasks } from "./inference"
+
+const TASK_CONCURRENCY = 1
+
+const queue = new PQueue({ concurrency: TASK_CONCURRENCY })
+
+const scheduleBackgroundInferenceTasks = async () => {
+  for (const task of backgroundInferenceTasks) {
+    console.log("scheduling task", task.name)
+    queue.add(async () => task())
+  }
+}
+
+queue.add(() => scheduleBackgroundInferenceTasks())
+queue.on("idle", async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  scheduleBackgroundInferenceTasks()
+})
 
 chrome.action.onClicked.addListener((tab) => {
   chrome.sidePanel.open({ windowId: tab.windowId })
